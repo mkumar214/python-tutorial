a3846dbca65f54df91fe3c6b02633af3bd583a93
kumarm <mukesh.kumar@tekmindz.com>
2019-11-11 15:07:27 +0530

    First commit

---
+++ basic/oops/classes/Shark.py
@@ -0,0 +1,12 @@
+class Shark :
+ def swim(self) :
+     print('shark is swimming')
+
+def main() :
+    shark1 = Shark()
+    print('Object is created')
+    shark1.swim()
+
+if __name__== "_main_" :
+   print('main is called')
+   main()
